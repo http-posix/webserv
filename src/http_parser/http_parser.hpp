5d42fb8a162bc5d1bd26a10030bd7f17de61f61a
Terra@42 <studentbreus@gmail.com>
2026-05-15 16:43:51 +0200

    feat: adding HttpParser .hpp file for current .cpp corresponder

---
+++ src/http_parser/http_parser.hpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <map>
+#include "../http_request/http_request.hpp"
+
+class HttpParser
+{
+	private:
+		std::string input_buffer_;
+		size_t input_i_;
+		HttpParserIntState state_;
+		HttpParserState external_state_;
+		
+		HttpMethod method_;
+		std::string path_;
+		std::string version_;
+		std::map<std::string, std::string> headers_;
+		std::string body_;
+		
+		int ParseRequestLine();
+		int ParseMethod(std::string str);
+		int ParseVersion(std::string str);
+		int ParseHeaders();
+		int ParseBody();
+	public:
+		HttpParser(void);
+		HttpParser(const HttpParser& other);
+		HttpParser& operator=(const HttpParser& other);
+		~HttpParser(void);
+
+		HttpParserState Feed(char *str);
+};
