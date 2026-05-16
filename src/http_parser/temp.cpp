501cd81ecfad0aac7783f8881ade40f9ac54399e
Terra@42 <studentbreus@gmail.com>
2026-05-16 20:35:54 +0200

    test(zach): Add test main for intermediate testing
    DO NOT INCLUDE LATER ON

---
+++ src/http_parser/temp.cpp
@@ -0,0 +1,29 @@
+#include "http_parser.hpp"
+#include <iostream>
+
+/*
+ * GET /index.html HTTP/1.1\r\n
+ * Host: example.com\r\n
+ * User-Agent: CustomClient/1.0\r\n
+ * Accept: \r\n
+ * \r\n
+*/
+/*
+ * POST /api/login HTTP/1.1\r\n
+ * Host: api.example.com\r\n
+ * Content-Type: application/json\r\n
+ * Content-Length: 42\r\n
+ * \r\n
+ * {"username":"alice","password":"secret"}
+*/
+
+int	main(void)
+{
+	HttpParser	test;
+
+	test.Feed("GET /index.html HTTP/1.0");
+	test.PrintParserRequest();
+	test.Feed("\r\n");
+	test.PrintParserRequest();
+	return (0);
+}
