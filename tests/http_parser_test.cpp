4ead5162de79e570fc38ec2907b6e573f74400cb
Terra@42 <studentbreus@gmail.com>
2026-06-09 15:18:46 +0200

    test: added Test for Valid Simple Get HttpRequest

--- tests/http_parser_test.cpp
+++ tests/http_parser_test.cpp
@@ -15,3 +15,21 @@ TEST_CASE("InvalidMethod") {
 	CHECK(p.GetInternalState() == Error);
 	CHECK(p.GetExternalState() == InvalidRequest);
 }
+
+TEST_CASE("Valid Simple GET Request") {
+	HttpParser p;
+
+	p.Feed("GET / HTTP/1.1\r\n");
+	p.Feed("Connection: Close\r\n");
+	p.Feed("Host: google.com\r\n");
+	p.Feed("Accept: */*\r\n");
+	p.Feed("User-agent: iamroot.tech\r\n");
+	p.Feed("\r\n");
+
+	CHECK(p.GetMethod() == Get);
+	CHECK(p.GetExternalState() == Complete);
+	CHECK(p.GetInternalState() == Done);
+	CHECK(p.GetBodyExpectedLength() == 0);
+	CHECK(p.GetVersion() == "HTTP/1.1");
+}
+
