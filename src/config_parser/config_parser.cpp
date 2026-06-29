9ac9a26e8d1202846699fcb65195eceaa45b3199
TerraMint <studentbreus@gmail.com>
2026-06-29 15:40:24 +0200

    feat(configParser): added canon implementation

---
+++ src/config_parser/config_parser.cpp
@@ -0,0 +1,17 @@
+ConfigParser::ConfigParser(void) {
+	
+}
+
+ConfigParser::ConfigParser(const ConfigParser& other) {
+	*this = other;
+}
+
+ConfigParser& ConfigParser::operator=(const ConfigParser& other) {
+	if (this != &other) {
+		
+	}
+	return *this;
+}
+
+ConfigParser::~ConfigParser(void) {
+}
