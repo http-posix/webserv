db5ecf9ad457bdc75c5ea87965d54986bfc08796
TerraMint <studentbreus@gmail.com>
2026-06-29 15:41:57 +0200

    progress(configParser): Added basic reading/opening file function

--- src/config_parser/config_parser.cpp
+++ src/config_parser/config_parser.cpp
@@ -2,6 +2,21 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+int ConfigParser::readFile(const std::string& filename)
+{
+	std::ifstream		inputFile(filename.c_str());
+	std::stringstream	buffer;
+
+	if (!inputFile.is_open())
+		return (-1);
+	buffer << inputFile.rdbuf();
+	file_string_ = buffer.str();
+	if (file_string_.empty())
+		return (-1);
+
+	return (0);
+}
+
 ConfigParser::ConfigParser(void) {
 	
 }
