b588aebdbb296a63485822709db5bad6fc7b88fb
Terra@42 <studentbreus@gmail.com>
2026-06-30 14:57:54 +0200

    test(configParser): testcase for non existing file

--- tests/config_parser.cpp
+++ tests/config_parser.cpp
@@ -11,6 +11,14 @@ TEST_CASE("Open and print content config file")
 	REQUIRE(p.readFile(file) == 0);
 }
 TEST_CASE("Open file with insufficient permissions")
+TEST_CASE("Open non existing file")
+{
+	ConfigParser p;
+	std::string file = "tests/config_test_files/nonexistingfile";
+
+	CHECK(p.readFile(file) != 0);
+}
+
 TEST_CASE("Open directory instead of file")
 {
 	ConfigParser p;
