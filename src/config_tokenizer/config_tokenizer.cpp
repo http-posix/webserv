8c485f0630731f1c5cc0e17b87abffba32b864bc
Terra@42 <studentbreus@gmail.com>
2026-07-10 09:09:58 +0200

    refactor(ConfigParser): adding comments for clarification

--- src/config_tokenizer/config_tokenizer.cpp
+++ src/config_tokenizer/config_tokenizer.cpp
@@ -4,6 +4,7 @@
 ConfigTokenizer::ConfigTokenizer(const std::string& input) 
 	: src(input), pos(0) {}
 
+// Technically a helper function and does not need to be part of the class
 bool ConfigTokenizer::isKeyword(std::string word)
 {
 	if (word == "server" || word == "location" || word == "listen" || word == "error_page" || word == "client_max_body_size" || word == "methods" || word == "return" || word == "root" || word == "autoindex" || word == "index" || word == "upload_enable" || word == "upload_store")
@@ -16,8 +17,10 @@ ConfigToken ConfigTokenizer::Next()
 	// Skip Optional White Space (OWS)
 	while (pos < src.size() && std::isspace(src[pos])) ++pos;
 
+	// Check for End of File.
 	if (pos >= src.size()) return {ConfigToken::EndOfFile, ""};
 	
+	// If word starts with alphabetical char, assume Keyword or Identifier
 	if (std::isalpha(src[pos]))
 	{
 		size_t start = pos;
@@ -29,6 +32,7 @@ ConfigToken ConfigTokenizer::Next()
 			return {ConfigToken::Identifier, word};
 	}
 
+	// NB: We do not convert number (yet) in tokenizer
 	if (std::isdigit(src[pos]))
 	{
 		size_t start = pos;
