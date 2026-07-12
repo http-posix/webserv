5b96d8d9dae75747aa903a9ee840345906060ee9
tetiana-cherni <tetiana.cherni@gmail.com>
2026-07-12 15:44:24 +0200

    feat(event_loop): implement signal handler, add tests, wire into main.cpp

---
+++ tests/signal_handler_test.cpp
@@ -0,0 +1,52 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "event_loop/signal_handler.hpp"
+#include <csignal>
+#include <unistd.h>
+
+TEST_SUITE("SignalHandling") {
+
+    TEST_CASE("Handle SIGINT shutdown") {
+        Signal::g_signal_running = 1;
+        Signal::SetupSignalHandlers();
+
+        std::raise(SIGINT);
+
+        CHECK(Signal::g_signal_running == 0);
+    }
+
+    TEST_CASE("Handle SIGTERM shutdown") {
+        Signal::g_signal_running = 1;
+        Signal::SetupSignalHandlers();
+
+        std::raise(SIGTERM);
+
+        CHECK(Signal::g_signal_running == 0);
+    }
+
+    TEST_CASE("SIGPIPE is ignored") {
+        Signal::SetupSignalHandlers();
+        int pipe_fds[2];
+        if (pipe(pipe_fds) == 0) {
+            
+			close(pipe_fds[0]); // close read-end
+			
+			// if SIGPIPE doesn't ignored, next line kill the process
+            ssize_t bytes = write(pipe_fds[1], "test", 4);
+
+            CHECK(bytes == -1);
+            CHECK(errno == EPIPE);
+            close(pipe_fds[1]);
+        }
+    }
+
+    TEST_CASE("Signals reset to default") {
+        Signal::SetupSignalHandlers();
+        Signal::SetupDefaultSignals();
+        
+        struct sigaction sa;
+        sigaction(SIGINT, nullptr, &sa);
+
+        CHECK(sa.sa_handler == SIG_DFL);
+    }
+}
