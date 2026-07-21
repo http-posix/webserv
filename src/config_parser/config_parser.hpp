f315033cecec6af02b32d0569e0442743455197e
Terra@42 <studentbreus@gmail.com>
2026-07-21 14:48:14 +0200

    feat(configParser): added prototype of locationConfig structure

--- src/config_parser/config_parser.hpp
+++ src/config_parser/config_parser.hpp
@@ -1,5 +1,30 @@
 #pragma once
 #include <string>
+struct LocationConfig
+{
+	// URI: Uniform Resource Identifier -> path to the location
+	std::string uri_path;
+
+	// What methods are allowed in the (sub)directory
+	// GET, POST, DELETE
+	std::vector<std::string> allowed_methods;
+
+	// Each return codenumber has their own redirection page
+	// Somewhat similar to error_pages
+	std::pair<int, std::string> redirections;
+
+	// root (directory) to override default root of server
+	// If non is provided, the server's default is used
+	std::string root;
+
+	// Location where uploaded files while be uploaded.
+	std::string upload_location;
+
+	// TODO
+	// index
+	// autoindex
+};
+
 
 class ConfigParser {
 	private:
