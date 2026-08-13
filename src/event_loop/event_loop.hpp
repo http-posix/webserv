e3ef271c0c7481d6188a0a07ccab1ec53f4cf83a
Tetiana Cherniaieva <tetiana.cherni@gmail.com>
2026-08-13 17:54:21 +0200

    feat(event-loop): add EventLoop skeleton (accept + listener happy path)
    - HandleListener: accept via listeners, non-blocking via Socket::adopt
    - happy path only, error handling still TODO
    - HandleConnectionEvent: stub, waiting on Connection class
    - prep for testing config parser & http request parser via mock config

---
+++ src/event_loop/event_loop.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "server/server.hpp"
+#include <cstddef>
+#include "event_loop/poll_manager.hpp"
+#include "connection/connection.hpp"
+
+
+/// need to give locations from config to response
+
+class EventLoop{
+	public:
+		EventLoop() = delete;
+		explicit EventLoop(std::vector<Server> listeners);
+		~EventLoop() = default;
+
+		void run();
+
+	private:
+		std::vector<Server>					listeners_;
+		const size_t						size_listeners_;
+		static int							timeout_; //60-75 sec in NGNIX – we need ms(!)
+		PollManager							pm_;
+		std::unordered_map<int, Connection>	connections_;
+		std::unordered_map<int, int>		cgi_owners_;
+
+		enum class Severity{
+			NonCritical,
+			Critical
+		};
+
+		// methods
+		Severity	HandlePollError();
+		void		HandleWatched(int ready_count);//finished
+		void		HandleListener(pollfd poll_entry, size_t i);//still need to check error behavior
+		int			HandleConnectionEvent(pollfd poll_entry);
+		int			HandleCGI();
+		int			AcceptConnection(int listener);
+		void		RequestShutdown();
+
+};
+
