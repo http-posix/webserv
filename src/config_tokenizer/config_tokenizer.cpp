ced1cdec7e9b57e33ddc664e3e5b83d5cb418a5a
Terra@42 <studentbreus@gmail.com>
2026-08-22 15:12:50 +0200

    feat(configTokenizer): added helper function checkNext()
    Function checks next keyword without updating position of the tokenizer.
    It is used to check for certain keywords without collecting the data.
    (fe: see if next word is a number before you start collecting untill
    ';')

--- src/config_tokenizer/config_tokenizer.cpp
+++ src/config_tokenizer/config_tokenizer.cpp
@@ -11,7 +11,6 @@ bool ConfigTokenizer::isKeyword(std::string word)
 		return (true);
 	return (false);
 }
-
 ConfigToken ConfigTokenizer::Next()
 {
 	// Skip Optional White Space (OWS)
@@ -62,6 +61,63 @@ ConfigToken ConfigTokenizer::Next()
 		return {ConfigToken::Symbol, std::string(1, ch)};
 
 	// As a failsafe, we assume final pos_sibility to be Identifier
+	// Things such as ':' or '/' for paths or ports
+	return {ConfigToken::Identifier, std::string(1, ch)};
+}
+
+ConfigToken ConfigTokenizer::checkNext()
+{
+	size_t temp_pos;
+
+	temp_pos = pos_;
+	// Skip Optional White Space (OWS)
+	while (temp_pos < src_.size() && std::isspace(src_[temp_pos])) ++temp_pos;
+
+	// Check for End of File.
+	if (temp_pos >= src_.size()) return {ConfigToken::EndOfFile, ""};
+	
+	// If word starts with alphabetical char, assume Keyword or Identifier
+	if (std::isalpha(src_[temp_pos]))
+	{
+		size_t start = temp_pos;
+		while (temp_pos < src_.size() && (std::isalnum(src_[temp_pos]) || src_[temp_pos] == '_' || src_[temp_pos] == '/')) ++temp_pos;
+		std::string word = src_.substr(start, temp_pos - start);
+		if (isKeyword(word))
+			return {ConfigToken::Keyword, word};
+		else
+			return {ConfigToken::Identifier, word};
+	}
+
+	// NB: We do not convert number (yet) in tokenizer
+	if (std::isdigit(src_[temp_pos]))
+	{
+		size_t start = temp_pos;
+		while (temp_pos < src_.size() && isdigit(src_[temp_pos])) ++temp_pos;
+		return {ConfigToken::Number, src_.substr(start, temp_pos - start)};
+	}
+
+	if (src_[temp_pos] == '"' || src_[temp_pos] == '\'')
+	{
+		// Quote can either be ' or ", that's why we save it dynamically
+		char quote = src_[temp_pos];
+		size_t start = ++temp_pos;
+		while (temp_pos < src_.size() && src_[temp_pos] != quote)
+			++temp_pos;
+		std::string str = src_.substr(start, temp_pos - start);
+		if (temp_pos < src_.size()) 
+			++temp_pos;
+		return {ConfigToken::String, str};
+	}
+
+	// Final call we check for things such as {, } ; etc.
+	// (Single Character Tokens AKA Symbols)
+	char ch = src_[temp_pos];
+	// Skip char so we don't infinitely loop on it.
+	++temp_pos;
+	if (ch == '{' || ch == '}' || ch == ':')
+		return {ConfigToken::Symbol, std::string(1, ch)};
+
+	// As a failsafe, we assume final temp_possibility to be Identifier
 	// Things such as ':' or '/' for paths or ports
 	return {ConfigToken::Identifier, std::string(1, ch)};
 }
